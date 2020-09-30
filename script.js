7868df746c749f6c34fa01252071d6012a8599c2
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-09-30 11:27:04 +0200

    Add sort by rating feature in js

--- script.js
+++ script.js
@@ -68,3 +68,47 @@ function generateRestaurantsHTML(item) {
       }
     };
 };
+
+//Button and Function to show Highest Rated Restaurant (rating higher than 3)
+const goodRatingSection = document.getElementById('only-good-rating-container');
+const onlyGoodButton = document.getElementById('only-good-rating-button');
+onlyGoodButton.addEventListener("click", filterGoodRatingRestaurants);
+
+const goodRatingText = document.getElementById('good-rating-text');
+
+function filterGoodRatingRestaurants() {
+  mainContentContainer.style.display = 'none';  //Hides main content div showing all 10 restaurants
+  goodRatingText.style.display = 'block'; //and shows div where we generate only very good rated restaurants
+
+  const request = new Request(requestURL, {
+    headers: new Headers({
+      'Accept': 'application/json',
+      'user-key': `${API_KEY}`
+    })
+  });
+
+  fetch(request)
+  .then ((response) => {
+      return response.json();
+  })
+
+  .then((restaurantsObject) => {
+    const restaurantsArray = restaurantsObject.restaurants;
+    restaurantsArray.forEach((item) => {
+      //Condition that filters only Restaurants with Rating higher than 3
+      if(item.restaurant.user_rating.aggregate_rating >= 3) {
+        goodRatingSection.innerHTML += generateRestaurantsHTML(item);
+        onlyGoodButton.removeEventListener("click", filterGoodRatingRestaurants); //so button can't be clicked again after list is shown
+      }
+    });    
+  })
+
+  .catch((error) => {
+      console.log(error);
+  });
+};
+
+// Function to Restart the Search query
+const restart = () => {
+  document.location.href = "";
+};
